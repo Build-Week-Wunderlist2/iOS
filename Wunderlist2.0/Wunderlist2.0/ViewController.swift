9e0700bbd763475586cbb5b36aa5a5534b890f33
Claudia Maciel <clc_80@yahoo.com>
2020-06-17 19:04:26 -0700

    Empty Xcode project

---
+++ Wunderlist2.0/Wunderlist2.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Wunderlist2.0
+//
+//  Created by Claudia Contreras on 6/17/20.
+//  Copyright © 2020 thecoderpilot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
